2e2c658d4cfe89710d7f8e0cfcc7bf8696d77123
Flawden <topchiidv1998@gmail.com>
2024-10-28 19:05:02 +0300

    была создана базовая структура DTO и контроллеров

---
+++ src/main/java/ru/skypro/homework/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package ru.skypro.homework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+
+}
